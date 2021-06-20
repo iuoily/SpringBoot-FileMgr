63ae717d1b5ac3da4c7f473859912a220e5d869f
liutao <1323846705@qq.com>
2021-06-20 16:14:22 +0800

    文件下载完成

--- src/main/java/com/swjd/controller/FileDownloadController.java
+++ src/main/java/com/swjd/controller/FileDownloadController.java
@@ -1,7 +1,9 @@
 package com.swjd.controller;
 
 import java.io.File;
+import java.net.URLEncoder;
 import javax.servlet.http.HttpServletRequest;
+
 
 
 import org.apache.commons.io.FileUtils;
@@ -24,6 +26,8 @@ public class FileDownloadController {
             String path = "D:/新建文件夹/upload/";
             //创建该文件对象 File.separator代表文件间隔符("/"或“\”,解决不同平台兼容性问题)
             File file = new File(path + File.separator + filename);
+            //对文件名编码，防止中文乱码
+            filename = this.getFilename(request,filename);
             //设置响应头
             HttpHeaders headers = new HttpHeaders();
             //通知浏览器以下载的方式打开文件
@@ -35,4 +39,20 @@ public class FileDownloadController {
                     headers,
                     HttpStatus.OK);
         }
+    /**
+     * 根据浏览器的不同进行编码设置，返回编码后的文件名
+     */
+    public String getFilename(HttpServletRequest request,String filename)throws Exception{
+        //IE不同版本User-Agent中出现的关键词
+        String[] IEBrowserKeyWords={"MSID","Trident","Edge"};
+        //获取请求头信息
+        String userAgent = request.getHeader("User-Agent");
+        for (String keyWord : IEBrowserKeyWords) {
+            if (userAgent.contains(keyWord)) {
+                //IE内核浏览器，统一为UTDF-8显示
+                return URLEncoder.encode(filename, "UTF-8");
+            }
+        }
+            return new String(filename.getBytes("UTF-8"),"ISO-8859-1");
+    }
 }
